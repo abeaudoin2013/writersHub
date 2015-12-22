b8ed7ace88a4eeef7434717a3dfb20ccc55006c1
Andy <abeaudoin2013@gmail.com>
2015-12-22 12:36:21 -0500

    added the ability to turn story content into editable input field which saves the original text

--- writersHub.js
+++ writersHub.js
@@ -1,19 +1,109 @@
+allDocs = new Mongo.Collection('documents');
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  //body helpers and events
+
+  Template.body.helpers({
+
+    allUserDocs: function () {
+
+      return allDocs.find({}, {sort: {createdAt: -1}});
+
     }
+
   });
+
+  // Create Doc Template Helpers
+
+  Template.makeDoc.helpers({
+
+    test: function () {
+
+      console.log('doc helper test');
+
+    }
+
+  });
+
+  // Create Doc Template events 
+
+  Template.makeDoc.events({
+
+    'submit form': function (event) {
+
+      event.preventDefault();
+
+      var storyContent = event.target.storyContent.value,
+          currentUserId = Meteor.userId(),
+          currentUsername = Meteor.user().username;
+      
+      allDocs.insert({
+        storyContent: storyContent,
+        createdBy: currentUserId,
+        createdAt: new Date(),
+        username: currentUsername
+      });
+
+      //temporary. Once we have a User's full document, we don't want to clear the form obviously
+
+      event.target.storyContent.value = "";
+
+
+    }
+
+  });
+
+  //User's documents helpers
+
+  Template.userDoc.helpers({
+
+    isOwner: function () {
+
+      return this.owner === Meteor.userId();
+
+    }
+
+  });
+
+  Template.userDoc.events({
+
+    'click .delete': function () {
+
+      //this._id is the id of the item that this event is being called on
+
+      Meteor.call('deleteDoc', this._id);
+
+    },
+
+    'dblclick .storyContent': function () {
+
+      var objectText = allDocs.findOne(this._id).storyContent
+
+      var objectAsParagraph = document.getElementById(this._id);
+
+      var objectAsInputElement = document.createElement("input");
+
+      objectAsInputElement.name = 'editable'
+
+      objectAsInputElement.value = objectText;
+
+      objectAsParagraph.parentNode.replaceChild(objectAsInputElement, objectAsParagraph);
+
+
+
+       
+    }
+
+  });
+
+  
+
 }
 
 if (Meteor.isServer) {
@@ -21,3 +111,40 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+Meteor.methods({
+
+  deleteDoc: function (docId) {
+
+    allDocs.remove(docId);
+
+  }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
